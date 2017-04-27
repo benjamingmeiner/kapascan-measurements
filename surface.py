d7fdae714c382dd0d942438a704f01027c8d9cd7
Benjamin Gmeiner <benjamin.gmeiner@mailbox.org>
2017-04-27 11:52:26 +0200

    Add save / load to measurement / analysis scripts

--- surface.py
+++ surface.py
@@ -2,17 +2,28 @@ import sys, os
 sys.path.append(os.path.join(os.path.abspath(''), os.path.pardir, 'kapascan'))
 
 from measurement import Measurement
-import plot
+from plot import plot
+import numpy as np
 
+sensor = '2011'
 host = '192.168.254.173'
 serial_port = 'COM3'
-settings = {
-        'sampling_time': 0.05,
-        'data_points': 100,
-        'extent': ((1, 18, 0.1), (1, 18, 0.1))
-        }
 
-with Measurement(host, serial_port, **settings) as m:
-    m.interactive_mode()
-    #x, y, z = m.scan()
-    #plot.plot(x, y, z)
+settings = {
+    'sampling_time': 0.256,
+    'data_points': 100,
+    'extent': ((1, 1.5, 0.1), (1, 1.5, 0.1)),
+    'mode': 'absolute',
+    }
+
+m = Measurement(sensor, host, serial_port, settings)
+with m:
+    #m.interactive_mode()
+    x, y, z = m.scan()
+    plot(x, y, z)
+
+
+datadir = "data"
+filename = "test"
+for coord, data in zip(("x", "y", "z"), (x, y, z)):
+    np.save(os.path.join(datadir, filename + "_" + coord), data)
--- surface_analysis.py
+++ surface_analysis.py
@@ -1,14 +1,17 @@
 import os
 import numpy as np
 from plot import plot
+import matplotlib.pyplot as plt
 import analysis
 
-background = np.load(os.path.join("data", "surface_background.npy"))
-background2 = np.load(os.path.join("data", "surface_background2.npy"))
-sample = np.load(os.path.join("data", "surface_sample.npy"))
+datadir = "data"
 
-x = np.load(os.path.join("data", "surface_x.npy"))
-y = np.load(os.path.join("data", "surface_y.npy"))
+background = np.load(os.path.join(datadir, "surface_background.npy"))
+background2 = np.load(os.path.join(datadir, "surface_background2.npy"))
+sample = np.load(os.path.join(datadir, "surface_sample.npy"))
+
+x = np.load(os.path.join(datadir, "surface_x.npy"))
+y = np.load(os.path.join(datadir, "surface_y.npy"))
 z = 0.5 * background + 0.5 * background2 - sample
 
 noise = (background2 - background) * 0.00005
@@ -17,5 +20,7 @@ noise -= noise.mean()
 stepsize = x[1] - x[0]
 diameter = int(8.2 / stepsize + 0.5)
 sensor = analysis.sensor_function(diameter)
-z_restored = analysis.wiener(z, sensor, noise)
-plot(x, y, z_restored)
+z_reconstructed = analysis.wiener(z, sensor, noise)
+
+line = ((4, 4), (20.95, 17.95))
+fig = plot(x, y, z_reconstructed)
