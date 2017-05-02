970c7cd5d374789d858ac87595d38da1fb7f7cdb
Benjamin Gmeiner <benjamin.gmeiner@mailbox.org>
2017-05-02 22:30:17 +0200

    Update simple measurement to new API.

--- simple_measurement.py
+++ simple_measurement.py
@@ -4,10 +4,15 @@ sys.path.append(os.path.join(os.path.abspath(''), os.path.pardir, 'kapascan'))
 import controller
 import matplotlib.pyplot as plt
 
+sensor = '2011'
 host = '192.168.254.173'
+
 data_points = 2000
-sampling_time = 1  # ms
-c = controller.Controller(2000, host)
-data = c.acquire(data_points, sampling_time, channels=[0]) / 1000
+sampling_time = 0.256  # ms
+c = controller.Controller(sensor, host)
+
+with c:
+    with c.acquisition('continuous', sampling_time):
+        data = c.get_data(data_points, channels=[1]) / 1000
 
 plt.plot(data)
