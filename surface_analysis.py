40ab8f512519498ca1e2e23d4b3aabda7464bd39
doener <doenerkebap@mailbox.org>
2017-05-16 17:20:18 +0200

    Use venv import paths

--- surface_analysis.py
+++ surface_analysis.py
@@ -1,9 +1,6 @@
-import sys, os
-sys.path.append(os.path.join(os.path.abspath(''), os.path.pardir, 'kapascan'))
-
 import numpy as np
-from plot import plot
-import analysis
+from kapascan.plot import plot
+from kapascan import analysis
 import shelve
 
 datadir = "data"
