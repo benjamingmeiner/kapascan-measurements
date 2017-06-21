d5bf0ce55e3c5e3b900b91e7f766b1bec14d58d7
doener <doenerkebap@mailbox.org>
2017-06-21 10:31:46 +0200

    Add data view notebook and Data.

--- surface.py
+++ surface.py
@@ -14,33 +14,38 @@ settings = {
     'sensors': ['1739'],
     'sampling_time': 0.256,
     'data_points': 50,
-    'extent': ((19.5, 24.5, 0.015), (5, 9.5, 0.015)),
+    'extent': ((28, 31, 0.02), (10, 12, 0.02)),
     'mode': 'absolute',
-    'direction': ('x', 'y'),
+    'direction': ('d', ),
     'change_direction': False
     }
 
 email_addr = "b.gmeiner@gmx.de"
+base_dir = "data"
+measurement_dir = "diagonal_0.02"
 
-for i in range(21, 50):
+data_dir = os.path.join(base_dir, measurement_dir)
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir, exist_ok=True)
 
-    email_subject = "Measurement {}".format(i)
+for i in range(1,10):
+    email_subject = "'{}': Measurement {}".format(measurement_dir, i)
     email_body = "Started at {}".format(datetime.datetime.now())
     m = Measurement(host_controller, serial_port, host_logger, settings)
     send(email_addr, email_subject, email_body)
     with m:
         x, y, z, T = m.scan()
 
-    datadir = "data/small"
     filename = "{:03d}".format(i)
     for coord, data in zip(("x", "y", "z", "T"), (x, y, z, T)):
-        np.save(os.path.join(datadir, filename + "_" + coord), data)
-    with shelve.open(os.path.join(datadir, filename + "_settings")) as file:
+        np.save(os.path.join(data_dir, filename + "_" + coord), data)
+    with shelve.open(os.path.join(data_dir, filename + "_settings")) as file:
         file['settings'] = m.settings
 
-    call("git add {}".format(datadir))
-    call('git commit -m "Data: Measurement {}."'.format(i))
-    call("git push")
+    call("git add {}".format(data_dir), shell=True)
+    call('git commit -m "Data: Measurement {}."'.format(i), shell=True)
+    call("git push", shell=True)
 
     email_body = "Finished at {}".format(datetime.datetime.now())
     send(email_addr, email_subject, email_body)
+
