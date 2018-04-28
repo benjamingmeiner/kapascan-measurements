beca96a57367de3142f553c5df533c800259f457
kapascan <kapascan@kapascan>
2018-04-28 15:28:33 +0200

    Make function available to save raw measurements from align.

--- utils/measure.py
+++ utils/measure.py
@@ -73,6 +73,7 @@ def align(settings, check_wipe=False):
             plot.plot(x, y, zi)
         return x, y, z, T, t
 
+
 @log_exception
 def align_raw(settings):
     c = Controller(settings['sensors'], host_controller)
@@ -89,12 +90,31 @@ def move():
     with Table(serial_port) as t:
         t.interact()
 
+
+@log_exception
+def save_raw(data, times, settings, directory, script_filename, i):
+    data_dir = os.path.join(base_dir, directory)
+    os.makedirs(data_dir, exist_ok=True)
+    prefix = _make_prefix(data_dir, i)
+    np.save(prefix + "data", data)
+    np.save(prefix + "time", times)
+    with shelve.open(prefix + "settings") as file:
+        file['settings'] = settings
+    logger.info(__("Written raw measurement data to {}.", prefix))
+    commit_message = "Raw Measurement {} in {}.".format(prefix[-4:-1], directory)
+    response = run([os.path.join(script_dir, "git.sh"),
+                    data_dir, script_filename, commit_message],
+                   stdout=PIPE, stderr=PIPE)
+    logger.info(__("Switched to branch {} and commited data.", directory))
+    if response.returncode != 0:
+        raise Exception(response.stderr.decode('utf-8'))
+
+
 @log_exception
 def raw_measure(settings, directory, script_filename, repeat=1):
     logger.info(__("Raw Measurement {}.", directory))
     logger.info(__("Acquiring data with settings:\n", settings, pretty=True))
-    data_dir = os.path.join(base_dir, directory)
-    os.makedirs(data_dir, exist_ok=True)
+
     c = Controller(settings['sensors'], host_controller)
     for i in range(1, repeat + 1):
         logger.info(__("Acquisition {} of {}:", i, repeat))
@@ -106,17 +126,4 @@ def raw_measure(settings, directory, script_filename, repeat=1):
             except ControllerError as error:
                 print(error)
                 return
-        prefix = _make_prefix(data_dir, i)
-        np.save(prefix + "data", data)
-        np.save(prefix + "time", [t0, t1])
-        with shelve.open(prefix + "settings") as file:
-            file['settings'] = settings
-        logger.info(__("Written raw measurement data to {}.", prefix))
-        commit_message = "Raw Measurement {} in {}.".format(i, directory)
-        response = run([os.path.join(script_dir, "git.sh"),
-                        data_dir, script_filename, commit_message],
-                       stdout=PIPE, stderr=PIPE)
-        logger.info(__("Switched to branch {} and commited data.", directory))
-        if response.returncode != 0:
-            raise Exception(response.stderr.decode('utf-8'))
     return c
